eee686d1f78425e7a2c7db505cb4678c5badd267
TeganHakim <tegan.hakim@gmail.com>
2025-07-03 00:03:06 -0400

    Started work on heatmap

--- src/static/js/data.js
+++ src/static/js/data.js
@@ -1,12 +1,12 @@
 async function loadHeatData() {
   // Example: Simulated points, or replace with API response
-  const points = [
-    [38.8951, -77.0364, 0.9],   // Washington DC, high sentiment
-    [51.5074, -0.1278, 0.6],    // London
-    [35.6895, 139.6917, 0.2],   // Tokyo
-    [30.0444, 31.2357, 0.8],    // Cairo
-  ];
-
+//   const points = [
+//     [38.8951, -77.0364, 0.9],   // Washington DC, high sentiment
+//     [37.8951, -76.0364, 0.6],    // London
+//     [38.5951, -77.6364, 0.2],   // Tokyo
+//     [39.8951, -78.0364, 0.8],    // Cairo
+//   ];
+//     var heat = L.heatLayer(points, {radius: 25}).addTo(map);
   // Add points to heatmap
 //   window.heatLayer.setLatLngs(points);
 }
